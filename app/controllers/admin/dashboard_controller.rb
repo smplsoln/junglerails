f1b5ad4527fdc058a96ed73e15a0ba19b5f957a3
Prashant R <smplsoln@gmail.com>
2021-11-12 07:30:31 -0600

    Add about paage and link in nav

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class Admin::DashboardController < ApplicationController
   def show
     @productscount = Product.all.count.to_s
-    puts 'Product count: ' + @productscount.to_s
+    # puts 'Product count: ' + @productscount.to_s
     @categoriescount = Category.all.count.to_s
-    puts 'Cetegories count: ' + @categoriescount.to_s
+    # puts 'Cetegories count: ' + @categoriescount.to_s
 
   end
 end
