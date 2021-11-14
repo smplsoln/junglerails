6612c13b0c4f307809779f08b649dfbbcea7debc
Prashant R <smplsoln@gmail.com>
2021-11-13 20:56:12 -0600

    Add user authentication has_secure_password

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,15 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # User signup routes
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
